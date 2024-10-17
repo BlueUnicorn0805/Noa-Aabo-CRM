74cee6ae1a8455c53c2019613de91a55f2733404
blueunicorn0805 <blueunicorn0805@gmail.com>
2024-10-17 21:25:16 +0700

    update : metaDescription => Aabo-CRM

--- resources/views/layouts/custom-app.blade.php
+++ resources/views/layouts/custom-app.blade.php
@@ -6,7 +6,7 @@
 		<meta charset="UTF-8">
 		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="description" content="Noa - Laravel Bootstrap 5 Admin & Dashboard Template">
+		<meta name="description" content="Aabo-CRM">
 		<meta name="author" content="Spruko Technologies Private Limited">
 		<meta name="keywords" content="laravel admin template, bootstrap admin template, admin dashboard template, admin dashboard, admin template, admin, bootstrap 5, laravel admin, laravel admin dashboard template, laravel ui template, laravel admin panel, admin panel, laravel admin dashboard, laravel template, admin ui dashboard">
 
